8093fb1d4fe934d2db266fe00e39abfa2de64567
npdevind <npdevind@gmail.com>
2024-05-12 22:40:57 +0530

    add js sample

--- mysql class/query.sql
+++ mysql class/query.sql
@@ -202,3 +202,9 @@ SELECT students.name, courses.course_name
 FROM students
 CROSS JOIN courses;
 
+
+-- INNER JOIN: Returns records that have matching values in both tables
+-- LEFT JOIN: Returns all records from the left table, and the matched records from the right table
+-- RIGHT JOIN: Returns all records from the right table, and the matched records from the left table
+-- CROSS JOIN: Returns all records from both tables
+
